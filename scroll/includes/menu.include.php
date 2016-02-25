247dbf8f1043f63c8963910656a1cac94aff71d8
Amanda Tavares <amandatvt@hotmail.com>
2016-02-25 15:58:13 -0300

    the begin

---
+++ scroll/includes/menu.include.php
@@ -0,0 +1,29 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">AJAT Desenvolvimento</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                <li><a href="#intro" class="page-scroll"><span></span>Início</a></li>
+                <li><a href="#about" class="page-scroll"><span></span>Trabalhos</a></li>
+                <li><a href="#services" class="page-scroll"><span></span>Depoimentos</a></li>
+                <li><a href="#partners" class="page-scroll"><span></span>Parceiros</a></li>
+                <!-- \<li><a href="#area-do-cliente"><span></span>Área do cliente</a></li> -->
+                <li><a href="#contact" class="page-scroll"><span></span>Contato</a></li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
